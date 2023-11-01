9baee75b8e6e777ad046b284d52ef915e55efcbb
Artem <Delysidcolor@gmail.com>
2023-11-01 14:10:34 +0400

    layouts, styles, some logic

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Header />
+  <Filters />
+
+  <main class="app-main">
+    <TodoList />
+    <AddTask />
+  </main>
+
+  <Footer />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "@/components/Header.vue";
+import Filters from "@/components/Filters.vue";
+import TodoList from "@/components/TodoList.vue";
+import AddTask from "@/components/AddTask.vue";
+import Footer from "@/components/Footer.vue";
+
+export default defineComponent({
+  components: {
+    Header,
+    Filters,
+    TodoList,
+    AddTask,
+    Footer,
+  },
+});
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul class="todo-list">
+    <TodoItem
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @toggleTodo="toggleTodo" />
+  </ul>
+</template>
+
+<script lang="ts">
+import { Todo } from "@/types/Todo";
+import { defineComponent } from "vue";
+import TodoItem from "@/components/TodoItem.vue";
+
+interface State {
+  todos: Todo[];
+}
+
+export default defineComponent({
+  components: {
+    TodoItem,
+  },
+  data(): State {
+    return {
+      todos: [
+        {
+          id: 0,
+          text: "Learn the basics of Typescript",
+          completed: true,
+        },
+        {
+          id: 1,
+          text: "Learn Nuxt",
+          completed: false,
+        },
+        {
+          id: 2,
+          text: "Find the Job",
+          completed: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleTodo(id: number) {
+      const targetTodo = this.todos.find((todo: Todo) => todo.id === id);
+      if (targetTodo) {
+        targetTodo.completed = !targetTodo.completed;
+      }
+    },
+  },
+});
+</script>
